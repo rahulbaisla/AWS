510c83c6866a11b5fe43a9ede1531e7b8f45837e
Rahul Bansal <rahulbaisla@gmail.com>
2020-01-18 09:03:32 +1100

    create loop.sh
    simple bash script to loop

---
+++ scripts/loop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Before running check if the script has permissions to be executed if not run chmod +x loop.sh
+for (( i=0; i<=10; i++ ))
+do
+  # Replace with your logic
+  echo hello
+done
